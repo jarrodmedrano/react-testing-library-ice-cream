efb422261ddf8ad3949982288fca122373d72fe1
Jarrod Medrano <jmedran@gmail.com>
2022-07-09 10:58:56 -0600

    remove node modules from repo

--- src/pages/summary/test/SummaryForm.test.tsx
+++ src/pages/summary/test/SummaryForm.test.tsx
@@ -1,62 +1,67 @@
-import { render, screen, fireEvent, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
-import {replaceCamelWithSpaces} from './utils';
-import { redColor, blueColor, disabledColor } from './utils';
-import userEvent from '@testing-library/user-event'
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { replaceCamelWithSpaces } from "../../../utils";
+import { redColor, blueColor, disabledColor } from "../../../utils";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 beforeEach(() => {
-   render(<SummaryForm />);
+  render(<SummaryForm />);
 });
 
-describe('Testing render',  () => {
-   test('Initial renders', async () => {
-       await waitFor(() =>   screen.getByRole('heading'));
+describe("Testing render", () => {
+  test("Initial renders", async () => {
+    await waitFor(() => screen.getByRole("heading"));
 
-    expect(screen.getByRole('heading')).toHaveTextContent('hello there');
+    expect(screen.getByRole("heading")).toHaveTextContent("hello there");
   });
-
 });
 
-describe('Test button is enabled after checkbox checked', () => {
-    test('Initial renders', async () => {
-      
-      const inputNode = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' });
-      const colorButton = screen.getByRole("button", { name: `Confirm order` });
-      
-      expect(colorButton).toBeDisabled(); 
-      
-      expect(inputNode).not.toBeChecked(); 
+describe("Test button is enabled after checkbox checked", () => {
+  test("Initial renders", async () => {
+    const inputNode = screen.getByRole("checkbox", {
+      name: "I agree to Terms and Conditions",
+    });
+    const colorButton = screen.getByRole("button", { name: `Confirm order` });
 
-      await userEvent.click(inputNode);
+    expect(colorButton).toBeDisabled();
 
-      expect(inputNode).toBeChecked(); 
+    expect(inputNode).not.toBeChecked();
 
-      expect(colorButton).toBeEnabled();  
+    await userEvent.click(inputNode);
 
-   }); 
+    expect(inputNode).toBeChecked();
+
+    expect(colorButton).toBeEnabled();
+  });
 });
 
-describe('testing popover', () => {
-  test('popover responds to hover', async () => {
+describe("testing popover", () => {
+  test("popover responds to hover", async () => {
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
 
-  // popover starts out hidden
-  const nullPopover = screen.queryByText(
-    /no ice cream will actually be delivered/i
-  );
-  expect(nullPopover).not.toBeInTheDocument();
+    // popover appears upon mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsAndConditions);
 
-  // popover appears upon mouseover of checkbox label
-  const termsAndConditions = screen.getByText(/terms and conditions/i);
-  userEvent.hover(termsAndConditions);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
 
-  const popover = screen.getByText(/no ice cream will actually be delivered/i);
-  expect(popover).toBeInTheDocument();
-
-  // popover disappears when we mouse out
-  userEvent.unhover(termsAndConditions);
-  await waitForElementToBeRemoved(() => 
-    screen.queryByText(/no ice cream will actually be delivered/i)
-  );
+    // popover disappears when we mouse out
+    userEvent.unhover(termsAndConditions);
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/no ice cream will actually be delivered/i)
+    );
+  });
 });
-
-})
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,8 +1,7 @@
- export const redColor = 'MediumVioletRed';
- export const blueColor = 'MidnightBlue';
- export const disabledColor = 'gray';
+export const redColor = "MediumVioletRed";
+export const blueColor = "MidnightBlue";
+export const disabledColor = "gray";
 
-
-  export function replaceCamelWithSpaces(colorName) {
-    return colorName.replace(/\B([A-Z])\B/g, ' $1');
-  }
+export function replaceCamelWithSpaces(colorName: string) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
